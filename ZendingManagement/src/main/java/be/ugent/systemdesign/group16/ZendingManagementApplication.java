aead432a652513769530851c6dcd7176455b8acc
Tristan De Groote <tristan.degroote@ugent.be>
2020-12-17 11:47:31 +0100

    ZendingManagement added

---
+++ ZendingManagement/src/main/java/be/ugent/systemdesign/group16/ZendingManagementApplication.java
@@ -0,0 +1,13 @@
+package be.ugent.systemdesign.group16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZendingManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZendingManagementApplication.class, args);
+	}
+
+}
