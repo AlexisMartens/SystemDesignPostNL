7164a7e0c1ffb5347c81a613573acaba49f76b4a
Alexis Martens Ubuntu <alexis.martens@ugent.be>
2020-12-20 15:32:29 +0100

    dockerfiles + bestel sql db + bestel ->zending

--- BestelManagement/src/main/java/be/ugent/systemdesign/group16/application/event/EventDispatcher.java
+++ BestelManagement/src/main/java/be/ugent/systemdesign/group16/application/event/EventDispatcher.java
@@ -2,10 +2,14 @@ package be.ugent.systemdesign.group16.application.event;
 
 import be.ugent.systemdesign.group16.domain.ExterneBestellingDomainEvent;
 import be.ugent.systemdesign.group16.domain.NieuweTrackAndTraceDomainEvent;
+import be.ugent.systemdesign.group16.domain.ZendingDomainEvent;
 
 public interface EventDispatcher {
 
 	void publishNieuweTrackAndTraceEvent(NieuweTrackAndTraceDomainEvent event);
 	
 	void publishExterneBestellingEvent(ExterneBestellingDomainEvent event);
+	
+	void publishZendingEvent(ZendingDomainEvent event);
+
 }
