290b67d1afa6f6f62ef9b7b0ee3b0ec1cdccb05b
student <tmerten.mertens@ugent.be>
2020-12-21 05:51:12 -0800

    Sorteer- en VervoerService toegevoegd

--- SorteerItemManagement/src/main/java/be/ugent/systemdesign/group16/domain/SorteerItem.java
+++ SorteerItemManagement/src/main/java/be/ugent/systemdesign/group16/domain/SorteerItem.java
@@ -1,15 +1,27 @@
 package be.ugent.systemdesign.group16.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class SorteerItem {
 	
 	private Integer sorteerItemId;
 	private Adres doel, afkomst;
 	private Soort soort;
-	private boolean inCentrum;
-	private boolean isBijLaatsteDepot;
+	private Adres huidigeLocatie;
+	private boolean isBijLaatsteLocatie;
+	
+	public void aangekomenOpNieuweLocatie(Adres l) {
+		this.huidigeLocatie=l;
+	}
+	
+	public void aangekomenOpLaatsteLocatie() {
+		// Maak nieuwe zending aan
+	}
+	
+	
 }
