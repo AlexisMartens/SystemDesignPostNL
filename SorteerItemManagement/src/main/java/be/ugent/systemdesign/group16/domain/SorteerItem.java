c65895f4b81d21274793345f38751aa8d3093328
student <tmerten.mertens@ugent.be>
2020-12-22 02:07:07 -0800

    Channels

--- SorteerItemManagement/src/main/java/be/ugent/systemdesign/group16/domain/SorteerItem.java
+++ SorteerItemManagement/src/main/java/be/ugent/systemdesign/group16/domain/SorteerItem.java
@@ -1,5 +1,7 @@
 package be.ugent.systemdesign.group16.domain;
 
+import java.time.LocalTime;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,12 +16,15 @@ public class SorteerItem {
 	private Soort soort;
 	private Adres huidigeLocatie;
 	private boolean isBijLaatsteLocatie;
+	private boolean spoed;
+	private LocalTime aanmaakDatum;
 	
 	public void aangekomenOpNieuweLocatie(Adres l) {
-		this.huidigeLocatie=l;
+		this.huidigeLocatie = l;
 	}
 	
-	public void aangekomenOpLaatsteLocatie() {
+	public void aangekomenOpLaatsteLocatie(Adres l) {
+		this.huidigeLocatie = l;
 		// Maak nieuwe zending aan
 	}
 	
