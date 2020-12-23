5377129cfb99704567068b8c6a1d281a1703a62b
Tristan De Groote <tristan.degroote@ugent.be>
2020-12-23 12:51:45 +0100

    update events messaging ZendingManagement

---
+++ ZendingManagement2/src/main/java/be/ugent/systemdesign/group16/application/event/BevestigAfleverenZendingEvent.java
@@ -0,0 +1,38 @@
+package be.ugent.systemdesign.group16.application.event;
+
+import java.time.LocalDate;
+
+import be.ugent.systemdesign.group16.domain.seedwork.DomainEvent;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class BevestigAfleverenZendingEvent extends DomainEvent {
+
+	private Integer orderId;
+	
+	private String naamAfzender;
+	private String postcodeAfzender;
+	private String straatAfzender;
+	private String plaatsAfzender;
+	private String landAfzender;
+	
+	private String naamOntvanger;
+	private String postcodeOntvanger;
+	private String straatOntvanger;
+	private String plaatsOntvanger;
+	private String landOntvanger;
+	
+	private boolean spoed;
+	
+	private boolean extern;
+	
+	private String orderStatus;
+
+
+}
---
+++ ZendingManagement2/src/main/java/be/ugent/systemdesign/group16/application/event/NieuweZendingDomainEvent.java
@@ -0,0 +1,37 @@
+package be.ugent.systemdesign.group16.application.event;
+
+import java.time.LocalDate;
+
+import be.ugent.systemdesign.group16.domain.seedwork.DomainEvent;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class NieuweZendingDomainEvent extends DomainEvent{
+		
+	private String naamAfzender;
+	private String postcodeAfzender;
+	private String straatAfzender;
+	private String plaatsAfzender;
+	private String landAfzender;
+	
+	private String naamOntvanger;
+	private String postcodeOntvanger;
+	private String straatOntvanger;
+	private String plaatsOntvanger;
+	private String landOntvanger;
+
+	private String naamHuidigeLocatie;
+	private String postcodeHuidigeLocatie;
+	private String straatHuidigeLocatie;
+	private String plaatsHuidigeLocatie;
+	private String landHuidigeLocatie;
+	
+	private String soort;
+	private boolean spoed;
+	private LocalDate aanmaakDatum;
+
+}
