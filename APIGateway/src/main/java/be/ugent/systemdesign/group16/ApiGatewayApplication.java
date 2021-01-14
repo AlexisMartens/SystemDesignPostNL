0a4a1313ed514dd092cc9509dca80e987a33421b
tmerten <tmerten.Mertens@UGent.be>
2021-01-14 22:31:37 +0100

    Update ApiGatewayApplication.java

--- APIGateway/src/main/java/be/ugent/systemdesign/group16/ApiGatewayApplication.java
+++ APIGateway/src/main/java/be/ugent/systemdesign/group16/ApiGatewayApplication.java
@@ -22,7 +22,7 @@ public class ApiGatewayApplication {
 				.route(r -> r.host("*").and().path("/api/fulfilmentklant/**").uri("http://localhost:2003"))
 				.route(r -> r.host("*").and().path("/api/koerier/**").uri("http://localhost:2004"))
 				.route(r -> r.host("*").and().path("/api/magazijnservice/**").uri("http://localhost:2005"))
-				.route(r -> r.host("*").and().path("/api/sorteeritem/brief").uri("http://localhost:2006"))
+				.route(r -> r.host("*").and().path("/api/sorteeritem/brief").uri("http://sorteeritem-service:2006"))
 				.route(r -> r.host("*").and().path("/api/trackandtrace/**").uri("http://localhost:2008"))
 				.route(r -> r.host("*").and().path("/api/zendingen/**").uri("http://localhost:2010"))
 				.build();
