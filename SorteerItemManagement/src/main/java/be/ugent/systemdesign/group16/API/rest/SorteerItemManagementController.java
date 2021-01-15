8f3c187b966f8a8fca02f387daf5997eecc2db97
tmerten <tmerten.Mertens@UGent.be>
2021-01-15 18:11:07 +0100

    Update SorteerItemManagementController.java

--- SorteerItemManagement/src/main/java/be/ugent/systemdesign/group16/API/rest/SorteerItemManagementController.java
+++ SorteerItemManagement/src/main/java/be/ugent/systemdesign/group16/API/rest/SorteerItemManagementController.java
@@ -17,7 +17,7 @@ import be.ugent.systemdesign.group16.application.SorteerItemService;
 import be.ugent.systemdesign.group16.domain.SorteerItem;
 
 @RestController
-@RequestMapping(path = "/api/sorteeritem/api/sorteeritem")
+@RequestMapping(path = "/api/sorteeritem")
 @CrossOrigin(origins = "*")
 public class SorteerItemManagementController {
 
