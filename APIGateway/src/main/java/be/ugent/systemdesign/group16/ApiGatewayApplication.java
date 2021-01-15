d7b7dbf4a4790a7ad2d16f29ec78742f0cdecdb1
Alexis Martens Ubuntu <alexis.martens@ugent.be>
2021-01-15 11:11:37 +0100

    swagger enable in api gw

--- APIGateway/src/main/java/be/ugent/systemdesign/group16/ApiGatewayApplication.java
+++ APIGateway/src/main/java/be/ugent/systemdesign/group16/ApiGatewayApplication.java
@@ -6,6 +6,9 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
 @SpringBootApplication
 public class ApiGatewayApplication {
 
